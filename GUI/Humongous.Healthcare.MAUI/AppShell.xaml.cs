1fef4ed3b7ec653f09bca472e1cd9c8ce256f4b4
Bill Reiss <billreiss@users.noreply.github.com>
2023-04-13 13:25:06 -0400

    Initial MAUI project

---
+++ GUI/Humongous.Healthcare.MAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Humongous.Healthcare.MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ GUI/Humongous.Healthcare.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Humongous.Healthcare.MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
