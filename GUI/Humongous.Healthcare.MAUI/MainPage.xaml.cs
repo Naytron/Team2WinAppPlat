5587ab60087a526fa0437e5c7d10840dbb90e086
Bill Reiss <billreiss@users.noreply.github.com>
2023-04-13 15:33:09 -0400

    Added logic to enter submission

--- GUI/Humongous.Healthcare.MAUI/MainPage.xaml.cs
+++ GUI/Humongous.Healthcare.MAUI/MainPage.xaml.cs
@@ -1,24 +1,56 @@
-﻿namespace Humongous.Healthcare.MAUI;
+﻿using Humongous.Healthcare.MAUI.Services;
+using System.Collections.ObjectModel;
+
+namespace Humongous.Healthcare.MAUI;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
-
+	SubmissionService svc = new SubmissionService();
+	ObservableCollection<string> symptoms = new ObservableCollection<string>();
+	string healthStatus = "";
 	public MainPage()
 	{
 		InitializeComponent();
+		symptomList.ItemsSource = symptoms;
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void OnSymptomButtonClicked(object sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		symptoms.Add(symptomEntry.Text);
+		symptomEntry.Text = "";
 	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		string[] symptoms = { "Congestion", "Cough", "Fever" };
+
+        var submission = new Models.Submission()
+		{
+			PatientID = 1,
+			Date = DateTime.Today,
+			HealthStatus = "I feel unwell",
+			Symptoms = symptoms,
+		};
+        bool ret = await svc.AddSubmission(submission);
+		if (ret)
+		{
+			await DisplayAlert("Submission", "Submitted successfully", "OK");
+		} 
+		else
+		{
+            await DisplayAlert("Submission", "Submission failed", "OK");
+        }
+    }
+
+    private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+		healthStatus = (sender as RadioButton).Value.ToString();
+    }
+
+    private void OnClearButtonClicked(object sender, EventArgs e)
+    {
+		symptoms.Clear();
+    }
 }
 
---
+++ GUI/Humongous.Healthcare.MAUI/Services/SubmissionService.cs
@@ -0,0 +1,46 @@
+﻿using Humongous.Healthcare.MAUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Humongous.Healthcare.MAUI.Services
+{
+    internal class SubmissionService
+    {
+        HttpClient _client;
+        JsonSerializerOptions _serializerOptions;
+        string uriPrefix = "";
+        public SubmissionService()
+        {
+            _client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+        }
+
+        public async Task<bool> AddSubmission(Submission sub)
+        {
+            string uri = uriPrefix;
+            string json = JsonSerializer.Serialize<Submission>(sub, _serializerOptions);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _client.PostAsync(uri, content);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<List<Submission>> GetAllSubmissions()
+        {
+            string uri = uriPrefix;
+            HttpResponseMessage response = await _client.GetAsync(uri);
+            var ret = JsonSerializer.Deserialize<List<Submission>>(response.Content.ToString(), _serializerOptions);
+            return ret;
+        }
+    }
+}
